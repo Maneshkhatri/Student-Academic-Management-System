a796e2272d6ccad68d52dc74ebe727196d112e2b
Maneshkhatri <2023n14128@gmail.com>
2026-09-16 01:43:30 -0700

    Add student menu function for management options

---
+++ student.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void studentMenu()
+{
+    printf("\n--- Student Management ---\n");
+    printf("1. Register Student\n");
+    printf("2. View Student\n");
+    printf("3. Search Student\n");
+    printf("4. Update Student\n");
+    printf("5. Delete Student\n");
+}

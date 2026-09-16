4d93058fa6eb86441706ede1a3a03b21bd033bcb
Mustafa Ali <mustafaalikeerio813@gmail.com>
2026-09-16 14:12:47 +0500

    Add marksMenu function for marks management

--- Marks.c
+++ Marks.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+void marksMenu()
+{
+    printf("\n--- Marks Management ---\n");
+    printf("1. Enter Marks\n");
+    printf("2. Calculate Total\n");
+    printf("3. Calculate Percentage\n");
+    printf("4. Calculate Grade\n");
+}

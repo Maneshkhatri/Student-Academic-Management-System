e3392f0901d0bf7d7089b188e51f8e04925ab027
Maneshkhatri <2023n14128@gmail.com>
2026-09-16 01:45:46 -0700

    Add courseMenu function for course management

---
+++ course.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void courseMenu()
+{
+    printf("\n--- Course Management ---\n");
+    printf("1. Register Course\n");
+    printf("2. Allocate Course\n");
+    printf("3. View Course Details\n");
+    printf("4. Exit Course Management\n");
+}
